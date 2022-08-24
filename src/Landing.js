a3d00797d60d3dfbdf18fefd9134f56ebe2e684a
maberer1 <madison.aberer@gmail.com>
2022-08-23 17:13:19 -0700

    Landing page styling in App.css

--- src/Landing.js
+++ src/Landing.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './App.css';
 
 function Landing(props) {
     return (
         <div>
-            <h1>Say It With Spotify</h1>
-            <h2>"Where words fail, music speaks."</h2>
-            <button>CREATE A PLAYLIST</button>
+            <h1 id="Header">Say It With Spotify</h1>
+            <h2 id="Tagline">"Where words fail, music speaks."</h2>
+            <button id="Create-A-Playlist-Btn">CREATE A PLAYLIST</button>
+            <h6 id="Author-Footer">Code by Madison Aberer</h6>
         </div>
     );
 }
