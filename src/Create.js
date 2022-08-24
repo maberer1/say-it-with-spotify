6f054f6497d5136258494bd31575cbb2a118d8be
maberer1 <madison.aberer@gmail.com>
2022-08-24 08:10:45 -0700

    Create page styling in App.css

--- src/Create.js
+++ src/Create.js
@@ -4,7 +4,8 @@ import './App.css';
 function Create(props) {
     return (
         <div>
-           <h1>Create</h1> 
+           <h1 id="Create-Prompt">Time to make the magic happen!</h1> 
+           <button id="Build-My-Playlist-Btn">BUILD MY PLAYLIST</button>
         </div>
     );
 }
