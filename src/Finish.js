7c82f3ca7899c07c118458b67fb819db0708ddfa
maberer1 <madison.aberer@gmail.com>
2022-08-24 08:14:59 -0700

    Finish page styling in App.css

--- src/Finish.js
+++ src/Finish.js
@@ -4,7 +4,10 @@ import './App.css';
 function Finish(props) {
     return (
         <div>
-            <h1>Finish</h1>
+            <h1 id="Success-Msg">Success! Your musical masterpiece is ready to be shared with the world.</h1>
+            <button id="View-My-Playlist-Btn">VIEW MY PLAYLIST</button>
+            <br></br>
+            <button id="New-Playlist-Btn">MAKE ANOTHER ONE!</button>
         </div>
     );
 }
